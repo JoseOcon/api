0214f2ae406976bb4c440b5930ff19f5b53ae7f7
JoseOcon <josebermudez587@gmail.com>
2021-10-22 23:47:17 -0600

    first commit

---
+++ routes/twitter.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+var HttpStatus = require("http-status-codes");
+const Twit = require("twit");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const T = new Twit({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+
+router.post("/", (req, res) => {
+  const message = req.body.message;
+
+  T.post("statuses/update", { status: message }, (err, reply) => {
+    if (err) {
+      console.log("Error: ", err.message);
+      res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err.message });
+    } else {
+        console.log("Success: ", reply);
+        res.sendStatus(HttpStatus.StatusCodes.CREATED);
+    }
+  });
+});
+
+module.exports = router;
